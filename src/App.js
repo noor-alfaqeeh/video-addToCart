c9679542f98f455c8743ddec6d28cc4899654079
Noor Alfaqeeh <noor.alfaqqeh#hotmail.com>
2019-08-07 09:57:35 +0300

    video

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Counter from '../src/component/counter'
+
+function App() {
+  return (
+    <div>   
+        <Counter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/counter.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = { 
+        value:this.props.value ,
+       
+     };
+    render() { 
+     
+
+        return ( 
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+                
+                <button 
+                onClick={() => this.props.onDelete(this.props.id)} 
+                className='btn btn-danger btn-sm m-2'>Delete</button>
+           
+            </div>
+         );
+    }
+    //........................<<<<<< methods >>>>>..........................
+
+    
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.value === 0) ? 'warning' : 'primary';
+        return classes;
+    }
+
+    formatCount(){
+        const {value: count}= this.state;
+        return count === 0 ? 'zero' : count;
+    }
+
+    handleIncrement= ()=>{
+        this.setState({ value: this.state.value +1 })
+   }
+  
+}
+ 
+export default Counter;
