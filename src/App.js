5f962534fcf836298c10687257b31a4a90f8fc44
Noor Alfaqeeh <noor.alfaqqeh#hotmail.com>
2019-08-07 14:58:11 +0300

    video

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './App.css';
-import Counter from '../src/component/counter'
+import Counters from "./component/counters"
+import NavBar from './component/navbar';
 
-function App() {
-  return (
-    <div>   
-        <Counter/>
-    </div>
-  );
+class App extends Component {
+  state = { 
+    counters: [
+        { id: 1, value: 0 },
+        { id: 2, value: 0 },
+        { id: 3, value: 0 },
+        { id: 4, value: 0 }
+    ]
+ };
+handleDelete=  counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+
+};
+
+handleReset= () => {
+    const counters = this.state.counters.map(c =>{
+        c.value = 0;
+        return c;
+    });
+    this.setState({ counters })
 }
 
+handleIncrement= counter =>{
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].value++;
+    this.setState({ counters });
+}
+  
+  render() { 
+    return ( 
+      <React.Fragment>
+      <NavBar totalCounters={this.state.counters.filter(c => c.value>0).length}/>
+      <main className='container'>
+        <Counters 
+        counters={this.state.counters}
+        onReset={this.handleReset}
+        onIncrement={this.handleIncrement}
+        onDelete={this.handleDelete}
+         />
+
+      </main>
+      </React.Fragment>
+
+     );
+  }
+}
+ 
 export default App;
--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = { 
-        value:this.props.value ,
-       
-     };
+   
     render() { 
      
 
         return ( 
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+                <button 
+                onClick={()=> this.props.onIncrement(this.props.counter)} 
+                className='btn btn-secondary btn-sm'>
+                    Increment</button>
+
                 
                 <button 
-                onClick={() => this.props.onDelete(this.props.id)} 
+                onClick={() => this.props.onDelete(this.props.counter.id)} 
                 className='btn btn-danger btn-sm m-2'>Delete</button>
            
             </div>
@@ -25,19 +26,15 @@ class Counter extends Component {
     
     getBadgeClasses() {
         let classes = 'badge m-2 badge-';
-        classes += (this.state.value === 0) ? 'warning' : 'primary';
+        classes += (this.props.counter.value === 0) ? 'warning' : 'primary';
         return classes;
     }
 
     formatCount(){
-        const {value: count}= this.state;
-        return count === 0 ? 'zero' : count;
+        const {value }= this.props.counter;
+        return value === 0 ? 'zero' : value;
     }
 
-    handleIncrement= ()=>{
-        this.setState({ value: this.state.value +1 })
-   }
-  
 }
  
 export default Counter;
